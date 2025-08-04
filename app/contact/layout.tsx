69d6427647857fe7f4ce45ca72ea78f4ae0f6fd3
Dhammika-Zent <senarathnadhammika89@gmail.com>
2025-08-04 09:54:21 +0530

    added page - contact

---
+++ app/contact/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Contact | Scholar Sync',
+  description: 'Get in touch with us for any questions or feedback.',
+};
+
+export default function ContactLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>;
+}
